e94ffede7ad0ddd9b63b0aef7e1b4e9fa9ce0f32
Jeffery Bernasko <jefferypeter20@gmail.com>
2022-12-07 10:03:38 +0000

    initial commit

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+// A Car class with attributes( basically variables) and methods
+//public is an access modifier that allows your attributes to be accessible outside the class
+
+public class Car {
+    public int doors;
+    public int wheels;
+    public String model;
+    public String color;
+
+    public String color(String black) {
+        this.color = color;
+        return color;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    Car toyota = new Car();
+
+    toyota.color = "Black";
+
+        System.out.println(toyota.color);
+    }
+
+}
